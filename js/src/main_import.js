5a1bd8d052b7d23b82cf8d0c1a96ac168e0c8e1c
cocoffee <cocoffee724@gmail.com>
2022-04-05 23:37:28 +0900

    [new] main_import.js

---
+++ js/src/main_import.js
@@ -0,0 +1,52 @@
+
+(function($){
+  
+  var win = $(window);
+  var body = $('body');
+  var headBox = $('#headBox');
+  var mainTab = $('#mainTab');
+  
+  var baseUrl = "../page/";
+  var importPage = ['head_Box.html','maintab.html'];
+  
+  
+  var tab = 1023;
+  var winWidth = win.outerWidth();
+  
+  // 스마트폰+타블렛 / 랩탑+피씨 디자인이 크기 제외 같기 때문에 1023 기준으로 나눔
+  // 새로고침 없이 outerWidth로 사이즈  스크립트 적용
+  
+  
+  var winWidth = $(window).width();
+    var nowDevice;
+    var afterDevice;
+    
+    var fnDevice = function(device){
+      device <= 1023 ? afterDevice = 'tablet' : afterDevice = 'pc';
+      if( nowDevice !== afterDevice){
+        nowDevice = afterDevice;
+      }
+    }//fnDevice()
+    fnDevice(winWidth);
+  
+    var fnDeviceHeader = function(device){
+      if( device === 'pc'){
+        headBox.load(baseUrl+importPage[0],function(){
+          body.append('<script src="../js/src/headBox_labtop_pc.js"></script>');
+        });
+        mainTab.load(baseUrl+importPage[1],function(){
+          body.append('<script src="../js/src/mainTab_labtop_pc.js"></script>');
+        });
+      }else{
+        headBox.load(baseUrl+importPage[0],function(){
+          body.append('<script src="../js/src/headBox_tablet_phone.js"></script>');
+        });
+        mainTab.load(baseUrl+importPage[1],function(){
+          body.append('<script src="../js/src/mainTab_tablet_phone.js"></script>');
+        });
+      }
+    }//fnDeviceHeader()
+  
+    fnDeviceHeader(nowDevice);
+  
+  })(jQuery);
