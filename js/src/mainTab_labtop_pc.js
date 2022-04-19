6bafbd4eda9c15fa6934e748603110b215209c46
cocoffee <cocoffee724@gmail.com>
2022-04-19 22:58:25 +0900

    [update] mainTab_lab_pc.js / 탭 메뉴 클릭 시 스크롤 이동 추가

--- js/src/mainTab_labtop_pc.js
+++ js/src/mainTab_labtop_pc.js
@@ -10,20 +10,61 @@
 
   var mainTabInner = document.querySelector('.main_tab_inner');
 
-  // mainTab.getBoundingClientRect().top = 현재 보이는 화면을 기준으로 #mainTab의 좌표값
-  //
+  var tabMenu = mainTabInner.querySelectorAll('button');
+
+  // y축으로 얼마나 스크롤(이동)되었는지 px좌표값을 반환
+  var tabTop = window.pageYOffset;
+  // console.log(tabTop);
+
+  
+  // mainTab.getBoundingClientRect().top = 현재 보이는 화면을 기준으로 #mainTab의 고정 좌표값
   var mainTabTop = window.pageYOffset + mainTab.getBoundingClientRect().top;
+  // console.log(mainTabTop);
 
+  // mainTab의 원래 시작점에서 이동한 만큼의 좌표값( mainTab(0) 보다 위로 올라가면 자연수값/ 내려가면 음수값 )
+  var mainTabTop = mainTab.getBoundingClientRect().top;
   // 화면 위치에 따라 값이 달라짐
-  console.log( mainTab.getBoundingClientRect().top);
+  // console.log(mainTabTop);
+  
 
-  function FnTabFixed(){
-    // 스크롤되는 값과 고정될 메뉴바 위치를 비교해서 고정시킬 class 적용 또는 해제 한다.
+
+
+  //함수_____________________________________________________
+
+
+  // 일정영역 이상 넘어갈 시 메인탭을 상단에 고정시키는 함수
+  function tabFixedFn(){
+    //스크롤값과 고정될 메뉴바 위치를 비교해서 고정시킬 class 적용 또는 해제 
     if ( window.pageYOffset >= mainTabTop ) {
       mainTab.classList.add("fixed");
     } else {
       mainTab.classList.remove("fixed");
-    }
-}
-// 스크롤이 되면 실행될 함수 지정
-document.addEventListener('scroll',FnTabFixed);
+    };
+  };//tabFixedFn()
+  
+  // 탭 메뉴 클릭시 mainTab article 영역 상단으로 이동한다.
+  var scrollTopFn = function(){
+    for(var i = 0; i < tabMenu.length; i++){
+      tabMenu[i].addEventListener('click',function(){
+          window.scrollTo({ top: 900, left: 0, behavior: 'smooth' });
+      });
+    };//for
+  };//scrollTopFn()
+  
+
+  // 스크롤이 되면 실행될 함수 지정
+  document.addEventListener('scroll',tabFixedFn);
+
+  scrollTopFn();
+
+
+  // 이벤트______________________________________________________
+
+// 탭 메뉴의 각 버튼을 클릭시 각 내용물 보여주기
+// 탭 메뉴 button css 수정하기
+
+
+
+  // ==========================================
+
+
